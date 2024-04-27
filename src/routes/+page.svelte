73aa14ff8569f01fc8c36b31964d7061f9d704a2
Daniel Dada <doesdeos@gmail.com>
2024-04-28 02:12:00 +0300

    upd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,8 +6,19 @@
 <AppShell regionPage="relative" slotPageHeader="sticky top-0 z-10">
 	<svelte:fragment slot="header">
 		<AppBar>
-			<h1 class="h1"><span class="gradient-heading">Under construction</span></h1>
+			<h1 class="h1"><span class="gradient-heading">Some dev stuff</span></h1>
 		</AppBar>
+		<div>
+			<h2 class="h2">
+				About
+			</h2>
+			<p>
+				Freaking metalhead and .NET developer. 
+				At the moment my interests are Rust, NixOS, modern .NET and Linux ricing 
+				(<a class="font-medium text-blue-600 dark:text-blue-500 hover:underline" href = "https://github.com/obsqrbtz/nixdots/">
+					my dots btw</a>).
+			</p>
+		</div>
 	</svelte:fragment>
 	<slot />
 	<!-- ---- / ---- -->
