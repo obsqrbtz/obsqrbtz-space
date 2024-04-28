9920e66830aac99104b2bbfc892800f64edbc294
Daniel Dada <doesdeos@gmail.com>
2024-04-28 15:37:07 +0300

    added systemd unit

--- dev.sh
+++ dev.sh
@@ -1,1 +1,7 @@
-npm run dev -- &
+#!/bin/bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+cd /home/user1/obsqrbtz-space
+npm run dev
