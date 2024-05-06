93121980bb5fb7aa81570f23ae3c1238b4358231
Daniel Dada <doesdeos@gmail.com>
2024-05-06 21:45:05 +0300

    updated email

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -11,5 +11,5 @@
 <h1>Contact</h1>
 
 <p>GitHub - <a rel="external" href="https://github.com/obsqrbtz">https://github.com/obsqrbtz</a></p>
-<p>Mail - <a rel="external" href="mailto:dan@obsqrbtz.space">dan@obsqrbtz.space</a></p>
+<p>Mail - <a rel="external" href="mailto:daniel@obsqrbtz.space">daniel@obsqrbtz.space</a></p>
 
