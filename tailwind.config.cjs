75eb4e71e2d96ba9bbad9d0cec5e3dc42cb90ad0
dada.daniel <doesdeos@gmail.com>
2024-11-05 16:29:10 +0300

    changed font, favicon and ajusted margins

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,8 +5,8 @@ module.exports = {
     darkMode: 'class',
     theme: {
         fontFamily: {
-            sans: ['Inter', ...defaultTheme.fontFamily.sans],
-            serif: ['Newsreader', ...defaultTheme.fontFamily.serif]
+            sans: ['JetBrains Mono', ...defaultTheme.fontFamily.sans],
+            serif: ['JetBrains Mono', ...defaultTheme.fontFamily.serif]
         },
         extend: {
             textColor: {
